0fc46f10a24f1aa4cb8207d2caf7ddfdc9f1dfc1
Pradyumna <pradyu99914@gmail.com>
2020-03-21 16:34:36 +0530

    added some comments

--- flaskapp.py
+++ flaskapp.py
@@ -8,6 +8,10 @@ def upload():
 @app.route('/makesubmission', methods = ["POST"])
 def submit():
     #create a new submission folder here.
+    #create a new folder with this things subid
+    #put all the files inside it
+    #run m.py on this submission thing
+    #have some flag to see if the submissiosnare ready
     print(request.files)
     return jsonify({}), 200
     
@@ -16,4 +20,4 @@ def success():
     return render_template("viewResults.html", name = request.args.get("subid"))  
   
 if __name__ == '__main__':  
-    app.run(debug = True, port = 1000)  
+    app.run(debug = True, port = 1000)
