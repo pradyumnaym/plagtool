8a7aa31f0f1fbbd5e3c68ca2db33b1dda4b0087c
Pradyumna <pradyu99914@gmail.com>
2020-03-24 23:20:50 +0530

    changes

--- flaskapp.py
+++ flaskapp.py
@@ -32,4 +32,4 @@ def success():
     return render_template("viewResults.html", name = request.args.get("subid"))  
 
 if __name__ == '__main__':  
-    app.run(debug = True, port = 1000)
+    app.run(debug = True, port = 1000, host = "0.0.0.0")
