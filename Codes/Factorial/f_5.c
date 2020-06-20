4c4b09ab8ea60fda9872dfd7eba1f6efd535ae78
Pradyumna <pradyu99914@gmail.com>
2020-06-20 18:23:11 +0530

    done with fact

---
+++ Codes/Factorial/f_5.c
@@ -0,0 +1,25 @@
+int factl(int n) 
+{ 
+    int res = 1, i; 
+    int p, q, r;
+    p = q;
+    q = r;
+    r = p;
+    for (i = 2; i <= n; i++) {
+        int p, q, r;
+    p = q;
+    q = r;
+    r = p;
+        res *= i; 
+
+    }
+    p = q;
+    q = r;
+    r = p;
+    return res; 
+} 
+  
+int main() 
+{ 
+    printf("Factorial of %d is %d", num, fact(5)); 
+} 
