04adff8868ae8eeafe84945f95b85500ce0f7f3e
Pradyumna <pradyu99914@gmail.com>
2020-06-20 18:11:30 +0530

    added new codes

---
+++ Codes/Fibonacci/fib_4.c
@@ -0,0 +1,16 @@
+int fib(int n) {
+	if(n <= 1) 
+		return n;
+	return fib(n-1) + fib(n-2);
+
+}
+
+int main() {
+	int pym = 1;
+	int avk = pym + 1;
+	int rn = pym + 2;
+
+	int n = 10;
+	int res = fib(n);
+	printf("%d\n",res);
+}
---
+++ Codes/Fibonacci/fib_6.c
@@ -0,0 +1,14 @@
+int fib(int);
+
+int main() {
+	int n = 10;
+	int res = fib(n);
+	printf("%d\n",res);
+}
+
+int fib(int n) {
+	if(n <= 1) 
+		return n;
+	return fib(n-1) + fib(n-2);
+
+}
