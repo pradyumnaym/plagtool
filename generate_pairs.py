f3101221bb6258e5258fd91877e83affe4eb139c
Pradyumna <pradyu99914@gmail.com>
2020-06-23 15:36:20 +0530

    chenged train test split

--- generate_pairs.py
+++ generate_pairs.py
@@ -7,35 +7,38 @@ from tf import generate_text_features
 
 import os.path as osp
 
-#read the merged features pickle dictionary
-with open("mfeat_mean.pkl", "rb") as f:
+with open(".\Feature_files\mfeat_mean.pkl", "rb") as f:
     d = pickle.load(f)
 
-#function to compute the euclidean distance between two vectors
 def euclidean_distance(v1, v2):
     return np.sqrt(np.sum(np.square(v1-v2)))
 
-#a dictionary that will hold all the assignment data
 assignments = {}
 
-#iterate through each assignment and generate features
 for assignment in d:
     labelarray = [0]    #0 - orig, 1 - plag, 2 - uplag
+
     asgnfeatures = np.zeros((0, 65), dtype = np.float32)
+
     asgnfeatures = np.concatenate(
         [asgnfeatures, 
         np.expand_dims(d[assignment]['orig'],0)
         ], 0)
 
+
     for submission in sorted(d[assignment]["plag"]):
+
         d[assignment]["plag"][submission] = np.expand_dims(d[assignment]["plag"][submission], 0)
         asgnfeatures = np.concatenate([asgnfeatures, d[assignment]["plag"][submission]], 0)
         labelarray.append(1)
+
     for submission in sorted(d[assignment]["uplag"]):
         d[assignment]["uplag"][submission] = np.expand_dims(d[assignment]["uplag"][submission], 0)
         asgnfeatures = np.concatenate([asgnfeatures, d[assignment]["uplag"][submission]], 0)
+        
         labelarray.append(2)
     assignments[assignment] = asgnfeatures
+
     assignments[assignment] = np.concatenate([assignments[assignment], np.expand_dims(np.asarray(labelarray, dtype = np.float32), 1)], 1)
 
 for assignment in assignments:
@@ -51,7 +54,12 @@ firstele = []
 secondele = []
 pair_u = []
 
-#generate pairs of codes for each assignment and create combined numpy arrays
+pairlabel_t = []
+pairrep_t = []
+firstele_t = []
+secondele_t = []
+pair_u_t = []
+
 for assignment in assignments:
     featmat = assignments[assignment]
 
@@ -63,36 +71,36 @@ for assignment in assignments:
         type1 = featmat[i][65]
 
         if(type1 == 0):
-            f1 = osp.join("D:\plagtool\Test_lance","IR_files",  "%s.ir.c"%assignment)
-            f1_u = osp.join("D:\plagtool\Test_lance",  "%s.c"%assignment)
+            f1 = osp.join("D:\plagtool\lance_ir_generator","Cleaned_IR",  "%s.ir.c"%assignment)
+            f1_u = osp.join("D:\plagtool\lance_ir_generator\C_files",  "%s.c"%assignment)
 
         elif(type1== 1):
             subid = list(sorted(list(d[assignment]["plag"].keys())))[i-1]
-            f1 =osp.join("D:\plagtool\Test_lance","IR_files", '%s_%s.ir.c'%(assignment, subid))
-            f1_u =osp.join("D:\plagtool\Test_lance", '%s_%s.c'%(assignment, subid))
+            f1 =osp.join("D:\plagtool\lance_ir_generator","Cleaned_IR", '%s_%s.ir.c'%(assignment, subid))
+            f1_u =osp.join("D:\plagtool\lance_ir_generator\C_files", '%s_%s.c'%(assignment, subid))
 
         else:
             subid =list(sorted(list(d[assignment]["uplag"].keys())))[i-1-len(list(d[assignment]['plag'].keys()))]
-            f1 = osp.join("D:\plagtool\Test_lance","IR_files",  '%s-%s.ir.c'%(assignment,subid))
-            f1_u = osp.join("D:\plagtool\Test_lance", '%s-%s.c'%(assignment,subid))
+            f1 = osp.join("D:\plagtool\lance_ir_generator","Cleaned_IR",  '%s-%s.ir.c'%(assignment,subid))
+            f1_u = osp.join("D:\plagtool\lance_ir_generator\C_files", '%s-%s.c'%(assignment,subid))
 
         
         for j in range(i+1, featmat.shape[0]):
             newfeats = featmat[j][:65]
             type2 = featmat[j][65]
             if(type2 == 0):
-                f2 = osp.join("D:\plagtool\Test_lance","IR_files",  "%s.ir.c"%assignment)
-                f2_u = osp.join("D:\plagtool\Test_lance",  "%s.c"%assignment)
+                f2 = osp.join("D:\plagtool\lance_ir_generator","Cleaned_IR",  "%s.ir.c"%assignment)
+                f2_u = osp.join("D:\plagtool\lance_ir_generator\C_files",  "%s.c"%assignment)
 
             elif(type2== 1):
                 subid = list(sorted(list(d[assignment]["plag"].keys())))[j-1]
-                f2 = osp.join("D:\plagtool\Test_lance","IR_files", '%s_%s.ir.c'%(assignment, subid))
-                f2_u = osp.join("D:\plagtool\Test_lance", '%s_%s.c'%(assignment, subid))
+                f2 = osp.join("D:\plagtool\lance_ir_generator","Cleaned_IR", '%s_%s.ir.c'%(assignment, subid))
+                f2_u = osp.join("D:\plagtool\lance_ir_generator\C_files", '%s_%s.c'%(assignment, subid))
 
             else:
                 subid =list(sorted(list(d[assignment]["uplag"].keys())))[j-1-len(list(d[assignment]['plag'].keys()))]
-                f2 = osp.join("D:\plagtool\Test_lance","IR_files",  '%s-%s.ir.c'%(assignment,subid))
-                f2_u = osp.join("D:\plagtool\Test_lance",  '%s-%s.c'%(assignment,subid))
+                f2 = osp.join("D:\plagtool\lance_ir_generator","Cleaned_IR",  '%s-%s.ir.c'%(assignment,subid))
+                f2_u = osp.join("D:\plagtool\lance_ir_generator\C_files",  '%s-%s.c'%(assignment,subid))
 
 
             diff = np.abs(pivot - newfeats)
@@ -101,18 +109,30 @@ for assignment in assignments:
             diff_u = np.concatenate([diff, np.asarray(textfeats_u, dtype = np.float32)], 0)
             diff = np.concatenate([diff, np.asarray(textfeats, dtype = np.float32)], 0)
 
-            firstele.append(pivot)
-            secondele.append(newfeats)
-            pairrep.append(diff)
-            pair_u.append(diff_u)
+            
 
-            if type1==0 and type2==1:
-                pairlabel.append(1)
-            elif type1==1 and type2==1:
-                pairlabel.append(1)
+            if assignment != "mul" and assignment !="pd":
+                firstele.append(pivot)
+                secondele.append(newfeats)
+                pairrep.append(diff)
+                pair_u.append(diff_u)
+                if type1==0 and type2==1:
+                    pairlabel.append(1)
+                elif type1==1 and type2==1:
+                    pairlabel.append(1)
+                else:
+                    pairlabel.append(0)
             else:
-                pairlabel.append(0)
-
+                firstele_t.append(pivot)
+                secondele_t.append(newfeats)
+                pairrep_t.append(diff)
+                pair_u_t.append(diff_u)
+                if type1==0 and type2==1:
+                    pairlabel_t.append(1)
+                elif type1==1 and type2==1:
+                    pairlabel_t.append(1)
+                else:
+                    pairlabel_t.append(0)
 
 pairlabel  =  np.asarray(pairlabel, dtype = np.float32)
 pairrep = np.asarray(pairrep, dtype = np.float32)
@@ -120,11 +140,18 @@ firstele =  np.asarray(firstele, dtype = np.float32)
 secondele =  np.asarray(secondele, dtype = np.float32)
 pair_u = np.asarray(pair_u, dtype  = np.float32)
 
+pairlabel_t  =  np.asarray(pairlabel_t, dtype = np.float32)
+pairrep_t = np.asarray(pairrep_t, dtype = np.float32)
+firstele_t =  np.asarray(firstele_t, dtype = np.float32)
+secondele_t =  np.asarray(secondele_t, dtype = np.float32)
+pair_u_t = np.asarray(pair_u_t, dtype  = np.float32)
+
+
 #normalize the textual features
 scaler = MinMaxScaler()
-pairrep[:, 66:] = scaler.fit_transform(pairrep[:, 66:])
+pairrep[:, 58:] = scaler.fit_transform(pairrep[:, 58:])
 scaler = MinMaxScaler()
-pair_u[:, 66:] = scaler.fit_transform(pair_u[:, 66:])
+pair_u[:, 58:] = scaler.fit_transform(pair_u[:, 58:])
 
 
 all_proc = {
@@ -132,7 +159,13 @@ all_proc = {
     "pair_rep" : pairrep, #incl textual features
     "firstele": firstele,
     "secondele" : secondele,
-    "pair_rep_u": pair_u
+    "pair_rep_u": pair_u,
+    "labels_t" : pairlabel_t,
+    "pair_rep_t" : pairrep_t, #incl textual features
+    "firstele_t": firstele_t,
+    "secondele_t" : secondele_t,
+    "pair_rep_u_t": pair_u_t
+
 }
 
 with open("D:\plagtool\\all_proc_u.pkl", "wb") as f:
--- generate_pairs_lance.py
+++ generate_pairs_lance.py
@@ -7,7 +7,7 @@ from tf import generate_text_features
 
 import os.path as osp
 
-with open("lmfeat_mean.pkl", "rb") as f:
+with open(".\Feature_files\lmfeat_mean.pkl", "rb") as f:
     d = pickle.load(f)
 
 def euclidean_distance(v1, v2):
@@ -54,6 +54,12 @@ firstele = []
 secondele = []
 pair_u = []
 
+pairlabel_t = []
+pairrep_t = []
+firstele_t = []
+secondele_t = []
+pair_u_t = []
+
 for assignment in assignments:
     featmat = assignments[assignment]
 
@@ -65,36 +71,36 @@ for assignment in assignments:
         type1 = featmat[i][57]
 
         if(type1 == 0):
-            f1 = osp.join("D:\plagtool\Test_lance","IR_files",  "%s.ir.c"%assignment)
-            f1_u = osp.join("D:\plagtool\Test_lance",  "%s.c"%assignment)
+            f1 = osp.join("D:\plagtool\lance_ir_generator","Cleaned_IR",  "%s.ir.c"%assignment)
+            f1_u = osp.join("D:\plagtool\lance_ir_generator\C_files",  "%s.c"%assignment)
 
         elif(type1== 1):
             subid = list(sorted(list(d[assignment]["plag"].keys())))[i-1]
-            f1 =osp.join("D:\plagtool\Test_lance","IR_files", '%s_%s.ir.c'%(assignment, subid))
-            f1_u =osp.join("D:\plagtool\Test_lance", '%s_%s.c'%(assignment, subid))
+            f1 =osp.join("D:\plagtool\lance_ir_generator","Cleaned_IR", '%s_%s.ir.c'%(assignment, subid))
+            f1_u =osp.join("D:\plagtool\lance_ir_generator\C_files", '%s_%s.c'%(assignment, subid))
 
         else:
             subid =list(sorted(list(d[assignment]["uplag"].keys())))[i-1-len(list(d[assignment]['plag'].keys()))]
-            f1 = osp.join("D:\plagtool\Test_lance","IR_files",  '%s-%s.ir.c'%(assignment,subid))
-            f1_u = osp.join("D:\plagtool\Test_lance", '%s-%s.c'%(assignment,subid))
+            f1 = osp.join("D:\plagtool\lance_ir_generator","Cleaned_IR",  '%s-%s.ir.c'%(assignment,subid))
+            f1_u = osp.join("D:\plagtool\lance_ir_generator\C_files", '%s-%s.c'%(assignment,subid))
 
         
         for j in range(i+1, featmat.shape[0]):
             newfeats = featmat[j][:57]
             type2 = featmat[j][57]
             if(type2 == 0):
-                f2 = osp.join("D:\plagtool\Test_lance","IR_files",  "%s.ir.c"%assignment)
-                f2_u = osp.join("D:\plagtool\Test_lance",  "%s.c"%assignment)
+                f2 = osp.join("D:\plagtool\lance_ir_generator","Cleaned_IR",  "%s.ir.c"%assignment)
+                f2_u = osp.join("D:\plagtool\lance_ir_generator\C_files",  "%s.c"%assignment)
 
             elif(type2== 1):
                 subid = list(sorted(list(d[assignment]["plag"].keys())))[j-1]
-                f2 = osp.join("D:\plagtool\Test_lance","IR_files", '%s_%s.ir.c'%(assignment, subid))
-                f2_u = osp.join("D:\plagtool\Test_lance", '%s_%s.c'%(assignment, subid))
+                f2 = osp.join("D:\plagtool\lance_ir_generator","Cleaned_IR", '%s_%s.ir.c'%(assignment, subid))
+                f2_u = osp.join("D:\plagtool\lance_ir_generator\C_files", '%s_%s.c'%(assignment, subid))
 
             else:
                 subid =list(sorted(list(d[assignment]["uplag"].keys())))[j-1-len(list(d[assignment]['plag'].keys()))]
-                f2 = osp.join("D:\plagtool\Test_lance","IR_files",  '%s-%s.ir.c'%(assignment,subid))
-                f2_u = osp.join("D:\plagtool\Test_lance",  '%s-%s.c'%(assignment,subid))
+                f2 = osp.join("D:\plagtool\lance_ir_generator","Cleaned_IR",  '%s-%s.ir.c'%(assignment,subid))
+                f2_u = osp.join("D:\plagtool\lance_ir_generator\C_files",  '%s-%s.c'%(assignment,subid))
 
 
             diff = np.abs(pivot - newfeats)
@@ -103,23 +109,42 @@ for assignment in assignments:
             diff_u = np.concatenate([diff, np.asarray(textfeats_u, dtype = np.float32)], 0)
             diff = np.concatenate([diff, np.asarray(textfeats, dtype = np.float32)], 0)
 
-            firstele.append(pivot)
-            secondele.append(newfeats)
-            pairrep.append(diff)
-            pair_u.append(diff_u)
+            
 
-            if type1==0 and type2==1:
-                pairlabel.append(1)
-            elif type1==1 and type2==1:
-                pairlabel.append(1)
+            if assignment != "mul" and assignment !="pd":
+                firstele.append(pivot)
+                secondele.append(newfeats)
+                pairrep.append(diff)
+                pair_u.append(diff_u)
+                if type1==0 and type2==1:
+                    pairlabel.append(1)
+                elif type1==1 and type2==1:
+                    pairlabel.append(1)
+                else:
+                    pairlabel.append(0)
             else:
-                pairlabel.append(0)
+                firstele_t.append(pivot)
+                secondele_t.append(newfeats)
+                pairrep_t.append(diff)
+                pair_u_t.append(diff_u)
+                if type1==0 and type2==1:
+                    pairlabel_t.append(1)
+                elif type1==1 and type2==1:
+                    pairlabel_t.append(1)
+                else:
+                    pairlabel_t.append(0)
 
 pairlabel  =  np.asarray(pairlabel, dtype = np.float32)
 pairrep = np.asarray(pairrep, dtype = np.float32)
 firstele =  np.asarray(firstele, dtype = np.float32)
 secondele =  np.asarray(secondele, dtype = np.float32)
 pair_u = np.asarray(pair_u, dtype  = np.float32)
+
+pairlabel_t  =  np.asarray(pairlabel_t, dtype = np.float32)
+pairrep_t = np.asarray(pairrep_t, dtype = np.float32)
+firstele_t =  np.asarray(firstele_t, dtype = np.float32)
+secondele_t =  np.asarray(secondele_t, dtype = np.float32)
+pair_u_t = np.asarray(pair_u_t, dtype  = np.float32)
 
 
 #normalize the textual features
@@ -134,7 +159,13 @@ all_proc = {
     "pair_rep" : pairrep, #incl textual features
     "firstele": firstele,
     "secondele" : secondele,
-    "pair_rep_u": pair_u
+    "pair_rep_u": pair_u,
+    "labels_t" : pairlabel_t,
+    "pair_rep_t" : pairrep_t, #incl textual features
+    "firstele_t": firstele_t,
+    "secondele_t" : secondele_t,
+    "pair_rep_u_t": pair_u_t
+
 }
 
 with open("D:\plagtool\\all_proc_u_lance.pkl", "wb") as f:
